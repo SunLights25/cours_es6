ef154c3b195718e59b102d210c021fdb5228fda5
SunLights25 <nikkohamon@hotmail.com>
2024-04-30 15:34:31 +0200

    tableau

--- js/app.js
+++ js/app.js
@@ -51,4 +51,44 @@ while(i < langs.length){
         console.log(cle.nom+ ' '+cle.prenom)
     })
 
+    //opérateurs = + - * /  % < > ? ?? :
+    let age = 24
+    let annee = 2024
+    console.log(age% 2)
+
+    console.log(`Vous êtes né en ${annee - age}`)
+    age% 2 == 0 ? console.log("Nombre paire") : console.log("Nombre impaire")
+
+    let prix = 55 //ht 
+    let taxe = 5.5
+    console.log(`Le prix ttc est de ${prix + (prix*taxe /100)}`) //ttc
+
+
+    const product=[
+        {name: "car", price: 6000},
+        {name: "Bike", price: 150},
+        {name: "phone", price: 700},
+        {name: "Watch", price: 200},
+        {name: "pen", price: 8},
+        {name: "bag", price: 50},
+    ];
+    console.log(product) 
+
+    console.table(product)
+
+        console.log(Object.values(product[0]))
+        console.log(product[1].price)
+
+        /*product.forEach((cle)=>{
+           console.log(cle.name+ ` `+cle.price)
+        })*/
+
+        for(let data of product){
+            console.log(data.name)
+        }
+        
+        
+        const filterRedItems = product.filter((row) => row.price  < 100)
+        console.table(filterRedItems)
+
 })()
