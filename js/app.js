fb39651be0d1088dd9041bb13a56044ad0843787
SunLights25 <nikkohamon@hotmail.com>
2024-04-30 17:56:07 +0200

    js nex change

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 console.log("Hello");
 const title = "Cours Javascript";
 console.log(title);
+document.addEventListener("DOMContentLoaded",(e)=>{
+    const title = "Cours ES6";
+    console.log(title);
+})
